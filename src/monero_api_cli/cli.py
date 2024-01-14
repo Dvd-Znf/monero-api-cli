64e79e194b4e268a914be91d8f5b8f0885e8326a
Dvd-Znf <david.zanfir.trove@gmail.com>
2024-01-14 14:30:23 +0200

    Updated cli to use new ip validation function

--- src/monero_api_cli/cli.py
+++ src/monero_api_cli/cli.py
@@ -3,6 +3,7 @@ from prompt_toolkit import PromptSession
 from .version import __version__
 from .rpc_methods import rpc_methods
 from prompt_toolkit.styles import Style
+from .functions import ip_address_validation
 import re
 
 style = Style.from_dict({
@@ -43,24 +44,10 @@ def cli(address, port):
             elif user_input == "daemon-address":
                 print("Please enter a valid daemon address")####
                 
-
-                ipv4_pattern = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
-                port_pattern = r'^[1-9]\d{0,4}$|0$'
                 daemon_address = input()
 
-                if len((daemon_address.split(":"))) != 2:
-                    print("Invalid input format. Please use [IPv4 address]:[Port number] format.")
-                    exit(1)
+                address, port = ip_address_validation(daemon_address)
 
-                address, port = daemon_address.split(":")
-
-                if not re.match(ipv4_pattern, address):
-                    print("Invalid IPv4 address. Please use a valid IPv4 address.")
-                    exit(1)
-
-                if not re.match(port_pattern, port):
-                    print("Invalid port number. Please use a valid port number (1-65535).")
-                    exit(1)
             elif user_input in rpc_methods:####
                 rpc_methods[user_input](address, port)
             elif len(user_input) > 0 and not str.isspace(user_input):
